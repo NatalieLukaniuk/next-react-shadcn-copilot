c8d1039828021df104d126b684ba27e631dd2be2
Natalie Klid <nklid@deloittece.com>
2026-05-29 09:25:04 +0300

    error handling attempt

---
+++ src/app/chat/error.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+export default function ErrorBoundary(){
+    return <div>
+        <h1>Something went wrong.</h1>
+        <p>Unable to load chat details. Please try again later.</p>
+    </div>
+}
